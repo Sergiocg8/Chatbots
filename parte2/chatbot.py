74d30c30422a1702838bffde2f5cd29bb05c89a9
Sergio Cabrera Guerrero <58943933+Sergiocg8@users.noreply.github.com>
2022-01-19 14:42:01 +0100

    Subida definitiva

--- parte2/chatbot.py
+++ parte2/chatbot.py
@@ -29,55 +29,55 @@ def answer_question():
         print("El Betis va el 3º en liga")
         
     elif answerclass == "prox_rival_betis":
-        print("El próximo rival del Betis es el Celta de Vigo")
+        print("El próximo rival del Betis es el Espanyol")
         
     elif answerclass == "fecha_partido_betis":
-        print("La fecha del próximo partido del Betis es el 2/01/2022")
+        print("La fecha del próximo partido del Betis es el 21/01/2022")
         
     elif answerclass == "puesto_liga_madrid":
         print("El Madrid va el 1º en liga")
         
     elif answerclass == "prox_rival_madrid":
-        print("El próximo rival del Madrid es el Getafe")
+        print("El próximo rival del Madrid es el Elche")
         
     elif answerclass == "fecha_partido_madrid":
-        print("La fecha del próximo partido del Madrid es el 2/01/2022")
+        print("La fecha del próximo partido del Madrid es el 23/01/2022")
         
     elif answerclass == "puesto_liga_barcelona":
-        print("El Barcelona va el 7º en liga")
+        print("El Barcelona va el 6º en liga")
         
     elif answerclass == "prox_rival_barcelona":
-        print("El próximo rival del Barcelona es el Mallorca")
+        print("El próximo rival del Barcelona es el Alavés")
         
     elif answerclass == "fecha_partido_barcelona":
-        print("La fecha del próximo partido del Barcelona es el 2/01/2022")
+        print("La fecha del próximo partido del Barcelona es el 23/01/2022")
         
     elif answerclass == "puesto_liga_at_madrid":
-        print("El Atlético de Madrid va el 5º en liga")
+        print("El Atlético de Madrid va el 4º en liga")
         
     elif answerclass == "prox_rival_at_madrid":
-        print("El próximo rival del Atlético de Madrid es el Rayo Vallecano")
+        print("El próximo rival del Atlético de Madrid es el Valencia")
         
     elif answerclass == "fecha_partido_at_madrid":
-        print("La fecha del próximo partido del Atlético de Madrid es el 2/01/2022")
+        print("La fecha del próximo partido del Atlético de Madrid es el 22/01/2022")
         
     elif answerclass == "puesto_nba_lakers":
-        print("Los Lakers van el 9º en la NBA")
+        print("Los Lakers van el 8º en la NBA")
         
     elif answerclass == "prox_rival_lakers":
-        print("El próximo rival de los Lakers son los Rockest")
+        print("El próximo rival de los Lakers son los Pacers")
         
     elif answerclass == "fecha_partido_lakers":
-        print("La fecha del próximo partido de los Lakers es el 29/12/2021")
+        print("La fecha del próximo partido de los Lakers es el 20/01/2022")
         
     elif answerclass == "puesto_acb_madrid":
         print("El Madrid Basket va el 1º en la ACB")
         
     elif answerclass == "prox_rival_madrid_basket":
-        print("El próximo rival del Madrid Basket es el Barcelona")
+        print("El próximo rival del Madrid Basket es el Barcelona Basket")
         
     elif answerclass == "fecha_partido_madrid_basket":
-        print("La fecha del próximo partido del Madrid Basket es el 2/01/2022")
+        print("La fecha del próximo partido del Madrid Basket es el 23/01/2022")
         
     elif answerclass == "prox_campeonato_nadal":
         print("El próximo campeonato de Nadal es el Open de Australia")
